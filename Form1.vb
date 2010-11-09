50ce4867bf8872e3bae97b528db35a9d4af90643
artschwagerb <artschwagerb@my.uwstout.edu>
2010-11-09 12:12:50 -0600

    push origin master
    Signed-off-by: artschwagerb <waterskiaddict@gmail.com>

---
+++ Form1.vb
@@ -0,0 +1,53 @@
+﻿Imports System.Xml
+Imports Microsoft.Win32
+Imports System.Net
+Imports System.Threading
+
+Public Class Form1
+
+    Private Const SPI_SETDESKWALLPAPER As Integer = &H14
+    Private Const SPIF_UPDATEINIFILE As Integer = &H1
+    Private Const SPIF_SENDWININICHANGE As Integer = &H2
+    Private Declare Auto Function SystemParametersInfo Lib "user32.dll" (ByVal uAction As Integer, ByVal uParam As Integer, ByVal lpvParam As String, ByVal fuWinIni As Integer) As Integer
+    Const WallpaperFile As String = "c:\wallpaper.jpg"
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Timer1.Start()
+    End Sub
+
+    Private Sub btnRandom_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRandom.Click
+
+        Dim a As New Thread(AddressOf SetWallbaseWallpaper)
+        a.IsBackground = True
+        a.Start()
+    End Sub
+
+    Sub download(ByVal url As String)
+        Dim grabber As New WebClient()
+        'Dim myUri = New Uri(url)
+        grabber.DownloadFile(url, "C:\wallpaper.jpg")
+        'grabber.DownloadFile(url, "C:\wallpaper.jpg")
+        grabber.Dispose()
+    End Sub
+
+    Friend Sub SetWallbaseWallpaper()
+        btnRandom.Enabled = False
+        btnRandom.Text = "Downloading..."
+        Try
+            download(Scraper.ScrapeNewWallpaper())
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, "C:\wallpaper.jpg", SPIF_UPDATEINIFILE Or SPIF_SENDWININICHANGE)
+
+        Catch Ex As Exception
+
+            MsgBox("There was an error setting the wallpaper: " & Ex.Message)
+
+        End Try
+        btnRandom.Enabled = True
+        btnRandom.Text = "Random New Wallpaper"
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        GC.Collect()
+    End Sub
+End Class
---
+++ Scraper.vb
@@ -0,0 +1,87 @@
+﻿Imports System.Collections.Generic
+Imports System.Text
+Imports System.IO
+Imports System.Net
+Imports System.Text.RegularExpressions
+
+Module Scraper
+
+    Public Function ScrapeNewWallpaper()
+        ''read images from a url and display them in the console window
+        'Dim imageUrls As List(Of String) = GetAllImagesFromUrl("http://wallbase.net/random/all/eqeq/1920x1080/0/110/20")
+
+        'For Each img As String In imageUrls
+        '    MsgBox(img)
+        'Next
+
+        'read links from Wallbase.net random thumbnail page
+        Dim linkUrls As List(Of String) = GetAllLinksFromUrl("http://wallbase.net/random/all/eqeq/1920x1080/0/110/20")
+
+        For Each link As String In linkUrls
+            If link.StartsWith("http://wallbase.net/wallpaper/") Then
+
+                ''read images from Wallbase.net download page
+                Dim imageUrls As List(Of String) = GetAllImagesFromUrl(link)
+
+                For Each img As String In imageUrls
+                    If img.StartsWith("http://wallbase2.net/") Then
+                        Return img
+                    End If
+                Next
+
+            End If
+
+        Next
+
+        Return "http://lucidwebdesigns.com/wallpaper/404error.jpg"
+    End Function
+    ''' <summary>
+    ''' Given a web page url, it will retrieve the Html from that page and parse the image tags in that page
+    ''' </summary>
+    ''' <param name="url">The Web page url in this format "http;//www.msn.com"</param>
+    ''' <returns>Returns a list of image urls as strings based on the url of a Web page</returns>
+    Public Function GetAllImagesFromUrl(ByVal url As String) As List(Of String)
+        Dim urlList As New List(Of String)()
+        Dim rawHtml As String = [String].Empty
+        'read the contents of the web page into a string
+        Using sr As New StreamReader(New WebClient().OpenRead(url))
+            rawHtml = sr.ReadToEnd()
+            sr.Dispose()
+        End Using
+        'regular expression to part out <img> tags from the html
+        Dim regExPattern As String = "< \s* img [^\>]* src \s* = \s* [\""\']? ( [^\""\'\s>]* )"
+        Dim r As New Regex(regExPattern, RegexOptions.IgnoreCase Or RegexOptions.IgnorePatternWhitespace)
+        regExPattern = Nothing
+        Dim matches As MatchCollection = r.Matches(rawHtml)
+        r = Nothing
+
+        For Each m As Match In matches
+            urlList.Add(m.Groups(1).Value)
+        Next
+        matches = Nothing
+        Return urlList
+    End Function
+
+    Public Function GetAllLinksFromUrl(ByVal url As String) As List(Of String)
+        Dim urlList As New List(Of String)()
+        Dim rawHtml As String = [String].Empty
+        'read the contents of the web page into a string
+        Using sr As New StreamReader(New WebClient().OpenRead(url))
+            rawHtml = sr.ReadToEnd()
+            sr.Dispose()
+        End Using
+        'regular expression to part out <img> tags from the html
+        Dim regExPattern As String = "< \s* a [^\>]* href \s* = \s* [\""\']? ( [^\""\'\s>]* )"
+        Dim r As New Regex(regExPattern, RegexOptions.IgnoreCase Or RegexOptions.IgnorePatternWhitespace)
+        regExPattern = Nothing
+        Dim matches As MatchCollection = r.Matches(rawHtml)
+        r = Nothing
+
+        For Each m As Match In matches
+            urlList.Add(m.Groups(1).Value)
+        Next
+        matches = Nothing
+        Return urlList
+    End Function
+
+End Module
